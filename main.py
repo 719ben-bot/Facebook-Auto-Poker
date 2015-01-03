6a305cdd9df71ee1ec6cd5c6e8201f46fc0454ff
719Ben <BenWilliams4560@gmail.com>
2015-01-03 01:13:06 -0700

    Added Blacklist & User Class

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import requests
 
+#class for the users for the auto poker
+class user(object):
+   def __init__(self, id, name):
+      self.id = id
+      self.name = name
+
+#class for the autoPoker
 class autoPoker(object):
 
    #create the autoPoke instance
    def __init__(self, email, password):
+
+      #create a blank blacklist
+      self.blacklist = list()
+
+      #create a blank target list
+      self.pokeTargets = list()
 
       #create a session using requests to keep track of cookies/sessions
       self.session = requests.session()
@@ -40,7 +53,7 @@ class autoPoker(object):
          self.fb_dtsg = homepg_dtsg[1][9:21]
 
          #set the user id
-         self.user_id = homepg_userid[1].split('" /><input type="hidden" name="c_src')[0]
+         self.user_id = homepg_userid[1].split('"')[0]
 
       #if we did not loggin correctly
       else:
@@ -64,41 +77,89 @@ class autoPoker(object):
          #split up the text to get the ids
          idFind = pokePage.text.split('id="poke_live_item_')
 
-         #go through each id and add them to postTargets
-         for item in idFind[1:]:
-            pokeTarget = item.split('"><div class="bh bi">')[0]
-            pokeTargets.append(pokeTarget)
+         #split up the text to get the names
+         nameFind = pokePage.text.split('width="100" class="bl n" alt="')
 
-         #assign poke targets
+         #go through each id and add them to postTargets
+         for item in range(1,len(idFind)):
+
+            #get the user to be poked id
+            pokeId = idFind[item].split('"')[0]
+
+            #get the user to be poked name
+            pokeName = nameFind[item].split('"')[0]
+
+            #create a new poke target user
+            pokeTarget = user(pokeId, pokeName)
+
+            #if the user is someone we want to poke back
+            if pokeId not in self.blacklist:
+
+               #add the target to the array
+               pokeTargets.append(pokeTarget)
+
+         #assign poke targets array
          self.pokeTargets = pokeTargets
 
 
    #poke a single user
    def pokeUser(self, poke_target):
 
-      #input for the poke
-      data = ("__a=1&poke_target=" + poke_target + "&__user=" + self.user_id
-      + "&__dyn=7n8ajEyl35zoSt2u6aWizGomyp9ErghyWgSmEVFLFwxBxCbzESu48jhHximmey8"
-      + "szoyfw&fb_dtsg=" + self.fb_dtsg)
+      #if the user is not on the blacklist and logged in
+      if poke_target.id not in self.blacklist and self.loggedin:
 
-      #make the post request
-      self.session.post("https://www.facebook.com/pokes/inline/", data=data)
+         #input for the poke
+         data = ("__a=1&poke_target=" + poke_target.id + "&__user=" +
+            self.user_id + "&__dyn=7n8ajEyl35zoSt2u6aWizGomyp9ErghyWgSmEV"
+            + "FLFwxBxCbzESu48jhHximmey8szoyfw&fb_dtsg=" + self.fb_dtsg)
 
-      #if the user we want to poke is in the pokeTargets
-      if poke_target in self.pokeTargets:
+         #make the post request
+         self.session.post("https://www.facebook.com/pokes/inline/", data=data)
 
-         #remove that id from the poke targets
-         self.pokeTargets.remove(poke_target)
+         #if the user we want to poke is in the pokeTargets
+         if poke_target in self.pokeTargets:
+
+            #remove that id from the poke targets
+            self.pokeTargets.remove(poke_target)
+
+         #return the user that was poked
+         return poke_target
 
 
    #poke all users back
    def pokeUsersBack(self):
 
+      #create a list to return
+      pokedList = list()
+
       #go though each of the ids in pokeTargets
-      for userid in self.pokeTargets:
+      for user in self.pokeTargets:
 
          #poke that user
-         self.pokeUser(userid)
+         self.pokeUser(user)
+
+         #add the user to the pokedList
+         pokedList.append(user)
+
+      #return a list of users that was poked
+      return pokedList
+
+   #add user to the blacklist
+   def addToBlacklist(self, poke_target):
+
+      #add to the blacklist
+      self.blacklist.append(poke_target)
+
+   #remove user from the blacklist
+   def removeFromBlacklist(self, poke_target):
+
+      #if the user is in the blacklist
+      if poke_target in self.blacklist:
+
+         #remove the user
+         self.blacklist.remove(poke_target)
+
+
 
 
 
@@ -111,4 +172,8 @@ myPoker = autoPoker("email@example.com", "password")
 myPoker.getPokeIds()
 
 #poke those users back
-myPoker.pokeUsersBack()
+usersPoked = myPoker.pokeUsersBack()
+
+#print the users poked
+for user in usersPoked:
+   print "Poked " + user.name
